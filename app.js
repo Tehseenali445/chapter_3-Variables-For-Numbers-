af65b14ec54c3682428a3027b322bcfa60d2a346
Tehseen Ali <154703743+Tehseenali445@users.noreply.github.com>
2024-03-10 23:44:48 +0500

    chapter_3 (Variables For Numbers)
    (Variables For Numbers)

---
+++ app.js
@@ -0,0 +1,25 @@
+// Q1
+
+// let age = "I am 29 yaer old"
+// alert(age)
+
+// Q2
+
+// let birth_year =  "<strong> '1996'<strong/>";
+// let detail = "My Birth Year Is   ";
+
+// document.write(detail , birth_year)
+
+// Q4
+
+// let visitor = "Tehseen Ali  ";
+// let order = "ordered ";
+// let Quantity = "8 ";
+// let Product = "T-Shirts ";
+// let oth = "on ";
+// let web = "<b>www.xyzClothing.com<b/> ";
+
+// document.write(visitor, order, Quantity, Product, oth, web);
+
+// Q5
+
